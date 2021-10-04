23c4995fe56456c9a445b7d3e33041b5f086f187
238M <91328698+238M@users.noreply.github.com>
2021-10-04 22:09:36 +0900

    Update 制作物内容.php

--- 制作物内容.php
+++ 制作物内容.php
@@ -125,7 +125,7 @@
 $dsn="データベース名";
 $user="ユーザー名";
 $password="パスワード";
-    $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 if(!empty($_POST["comment2"])){
     $name2=$_POST["name2"];
     $comment2=$_POST["comment2"];
